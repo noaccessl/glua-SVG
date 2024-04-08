f73d96b87e69f438c6e8c061fe0efb76fd37d47b
noaccessl <rodion.noaccessl@gmail.com>
2024-04-08 16:03:09 +0300

    Rework
    * New function: svg.AsyncLoad
    * Outsmart GitHub's Lua syntax highlight (Replaced minuses with dashes)
    * Refactor code structure
    * Style rebase & cleanups

--- main.lua
+++ main.lua
@@ -1,124 +1,182 @@
---[[---------------------------------------------------------------------------
-	Predefines
----------------------------------------------------------------------------]]
-svg = svg or {}
-svg.cache = svg.cache or {}
+--[[–––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+	Prepare
+–––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––]]
+local RemoveHandle = FindMetaTable( 'Panel' ).Remove
 
---[[---------------------------------------------------------------------------
-	Template
----------------------------------------------------------------------------]]
-local SVGTemplate = [[
-<html>
-	<head>
-		<style>
-			body {
-				margin: 0;
-				padding: 0;
-				overflow: hidden;
-			}
-		</style>
-	</head>
-	<body>
-		%s
-	</body>
-</html>
+local Assert	= assert
+local isnumber	= isnumber
+local isstring	= isstring
+
+local strfind	= string.find
+local substrof	= string.sub
+local strgsub	= string.gsub
+
+local Format	= string.format
+
+-- Helper function
+local SetIfEmpty do
+
+	local strlen = string.len
+
+	function SetIfEmpty( str, what, pos, needed )
+
+		if not strfind( str, what ) then
+			return substrof( str, 1, pos ) .. needed .. substrof( str, pos + strlen( needed ) )
+		end
+
+		return str
+
+	end
+
+end
+
+-- HTML wrap for SVGs
+local SVG_TEMPLATE = [[
+	<html>
+		<head>
+			<style>
+				body {
+					margin: 0;
+					padding: 0;
+					overflow: hidden;
+				}
+			</style>
+		</head>
+		<body>
+			%s
+		</body>
+	</html>
 ]]
 
 
-function svg.ClearCache()
+--[[–––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+	Initialize
+–––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––]]
+svg = svg or {
 
-	for id, handle in pairs( svg.cache ) do
+	Registry = {}
 
-		handle:Remove()
-		svg.cache[id] = nil
+}
+
+--[[–––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+	PurgeAll
+–––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––]]
+function svg.PurgeAll()
+
+	local Registry = svg.Registry
+
+	for id, pHandle in pairs( Registry ) do
+
+		RemoveHandle( pHandle )
+		Registry[ id ] = nil
 
 	end
 
 end
 
+--[[–––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+	Unload
+–––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––]]
 function svg.Unload( id )
 
-	if svg.cache[id] then
+	local Registry = svg.Registry
+	local pHandle = Registry[ id ]
 
-		svg.cache[id]:Remove()
-		svg.cache[id] = nil
+	if pHandle then
+
+		RemoveHandle( pHandle )
+		Registry[ id ] = nil
 
 	end
 
 end
 
-local function SetIfEmpty( str, what, pos, needed )
-
-	if not string.find( str, what ) then
-		return string.sub( str, 1, pos ) .. needed .. string.sub( str, pos + string.len( needed ) )
-	end
-
-	return str
-
-end
-
---[[---------------------------------------------------------------------------
+--[[–––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
 	Generate
----------------------------------------------------------------------------]]
+–––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––]]
 function svg.Generate( id, w, h, strSVG )
 
-	assert( isnumber( w ), 'invalid width' )
-	assert( isnumber( h ), 'invalid height' )
+	-- Pinch of asserts
+	Assert( isnumber( w ), 'invalid width' )
+	Assert( isnumber( h ), 'invalid height' )
 
-	assert( isstring( strSVG ), 'invalid svg' )
+	Assert( isstring( strSVG ), 'invalid svg' )
 
-	local open = string.find( strSVG, '<svg%s(.-)>' )
-	local _, close = string.find( strSVG, '</svg>%s*$' )
+	local open		= strfind( strSVG, '<svg%s(.-)>' )
+	local _, close	= strfind( strSVG, '</svg>%s*$' )
 
-	assert( ( open and close ) ~= nil, 'invalid svg' )
+	Assert( ( open and close ) ~= nil, 'invalid svg' )
 
-	strSVG = string.sub( strSVG, open, close )
+	-- Corrections
+	strSVG = substrof( strSVG, open, close )
 
 	strSVG = SetIfEmpty( strSVG, 'width="(.-)"', 5, 'width="" ' )
 	strSVG = SetIfEmpty( strSVG, 'height="(.-)"', 5, 'height="" ' )
 
-	strSVG = string.gsub( strSVG, 'width="(.-)"', 'width="' .. w .. '"' )
-	strSVG = string.gsub( strSVG, 'height="(.-)"', 'height="' .. h .. '"' )
+	strSVG = strgsub( strSVG, 'width="(.-)"', 'width="' .. w .. '"' )
+	strSVG = strgsub( strSVG, 'height="(.-)"', 'height="' .. h .. '"' )
 
-	local handle = svg.cache[id]
+	-- Cooking
+	local Registry = svg.Registry
+	local pHandle = Registry[ id ]
 
-	if not handle then
+	if not pHandle then
 
-		handle = vgui.Create( 'DHTML' )
-		handle:SetVisible( false )
+		pHandle = vgui.Create( 'DHTML' )
+		pHandle:SetVisible( false )
 
-		svg.cache[id] = handle
+		Registry[ id ] = pHandle
 
 	end
 
-	handle:SetSize( w, h )
-	handle:SetHTML( SVGTemplate:format( strSVG ) )
+	pHandle:SetSize( w, h )
+	pHandle:SetHTML( Format( SVG_TEMPLATE, strSVG ) )
 
-	return function( x, y, color, r, g, b, a )
-		svg.Draw( id, x, y, color, r, g, b, a )
+	return function( ... )
+
+		svg.Draw( id, ... )
+
 	end
 
 end
 
-
---[[---------------------------------------------------------------------------
+--[[–––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
 	Load
----------------------------------------------------------------------------]]
+–––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––]]
 function svg.Load( id, w, h, path )
 
 	local strSVG = file.Read( path, 'DATA' )
-	assert( strSVG ~= nil, 'invalid path' )
+	Assert( strSVG ~= nil, 'invalid path' )
 
 	return svg.Generate( id, w, h, strSVG )
 
 end
 
+--[[–––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+	AsyncLoad
+–––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––]]
+function svg.AsyncLoad( id, w, h, path )
+
+	file.AsyncRead( path, 'DATA', function( _, _, status, strSVG )
+
+		Assert( status == FSASYNC_OK, Format( 'Something went wrong. Status: %i', status ) )
+		svg.Generate( id, w, h, strSVG )
+
+	end )
+
+end
+
+--[[–––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+	LoadURL
+–––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––]]
 function svg.LoadURL( id, w, h, url )
 
-	assert( string.GetExtensionFromFilename( url ) == 'svg', 'invalid svg' )
+	Assert( string.GetExtensionFromFilename( url ) == 'svg', 'invalid svg' )
 
 	http.Fetch( url, function( strSVG )
+
 		svg.Generate( id, w, h, strSVG )
+
 	end, function( err )
 
 		error( err )
@@ -126,45 +184,45 @@ function svg.LoadURL( id, w, h, url )
 
 	end )
 
-	return function( x, y, color, r, g, b, a )
-		svg.Draw( id, x, y, color, r, g, b, a )
+	return function( ... )
+
+		svg.Draw( id, ... )
+
 	end
 
 end
 
-
---[[---------------------------------------------------------------------------
+--[[–––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
 	Draw
 
-		@color:
-			If set to true, it will create material that supports color.
-			It's not that expensive, but if you don't need color support, you can simply omit this argument.
+	@color:
+		If set to true, it will create material with color support.
+		It's not that expensive and if you don't need color support just omit this argument.
 
-		@r, g, b, a:
-			It works the same as in surface.SetDrawColor
----------------------------------------------------------------------------]]
+	@r, g, b, a:
+		Same principle as with surface.SetDrawColor
+–––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––]]
 do
 
-	local _R = debug.getregistry()
+	local PANEL				= FindMetaTable( 'Panel' )
+	local IMATERIAL			= FindMetaTable( 'IMaterial' )
 
-	local UpdateHTMLTexture = _R.Panel.UpdateHTMLTexture
-	local GetHTMLMaterial = _R.Panel.GetHTMLMaterial
+	local UpdateHTMLTexture	= PANEL.UpdateHTMLTexture
+	local GetHTMLMaterial	= PANEL.GetHTMLMaterial
 
-	local SetDrawColor = surface.SetDrawColor
-	local SetMaterial = surface.SetMaterial
-	local DrawTexturedRect = surface.DrawTexturedRect
+	local SetDrawColor		= surface.SetDrawColor
+	local SetMaterial		= surface.SetMaterial
+	local DrawTexturedRect	= surface.DrawTexturedRect
 
-	local MaterialWidth = _R.IMaterial.Width
-	local MaterialHeight = _R.IMaterial.Height
+	local WidthOf			= IMATERIAL.Width
+	local HeightOf			= IMATERIAL.Height
+	local NameOf			= IMATERIAL.GetName
 
-	local MaterialName = _R.IMaterial.GetName
+	local CreateMaterial	= CreateMaterial
 
-	local CreateMaterial = CreateMaterial
+	local strmatch			= string.match
 
-	local format = string.format
-	local match = string.match
-
-	local MaterialAttributes = {
+	local Attributes = {
 
 		[ '$translucent' ] = 1;
 		[ '$vertexalpha' ] = 1;
@@ -174,10 +232,10 @@ do
 
 	local function SetupMaterial( id, name, w, h )
 
-		MaterialAttributes[ '$basetexture' ] = name
+		Attributes[ '$basetexture' ] = name
 
-		local UniqueName = format( '%s,%s,%d,%d', id, match( name, '%d+' ), w, h )
-		return CreateMaterial( UniqueName, 'UnlitGeneric', MaterialAttributes )
+		local UniqueName = Format( '%s_%s_%d_%d', id, strmatch( name, '%d+' ), w, h )
+		return CreateMaterial( UniqueName, 'UnlitGeneric', Attributes )
 
 	end
 
@@ -185,38 +243,38 @@ do
 
 	function svg.Draw( id, x, y, color, r, g, b, a )
 
-		local handle = svg.cache[id]
+		local pHandle = svg.Registry[ id ]
 
-		if not handle then
+		if not pHandle then
 			return
 		end
 
-		UpdateHTMLTexture( handle )
+		UpdateHTMLTexture( pHandle )
 
-		local mat = GetHTMLMaterial( handle )
+		local pMaterial = GetHTMLMaterial( pHandle )
 
-		if not mat then
+		if not pMaterial then
 			return
 		end
 
-		local w = MaterialWidth( mat )
-		local h = MaterialHeight( mat )
-
-		SetDrawColor( 255, 255, 255 )
+		local w = WidthOf( pMaterial )
+		local h = HeightOf( pMaterial )
 
 		if color == true then
 
-			mat = SetupMaterial( id, MaterialName( mat ), w, h )
+			pMaterial = SetupMaterial( id, NameOf( pMaterial ), w, h )
 
 			if IsColor( r ) then
 				SetDrawColor( r.r, r.g, r.b, r.a )
-			elseif r then
+			else
 				SetDrawColor( r, g, b, a )
 			end
 
+		else
+			SetDrawColor( 255, 255, 255 )
 		end
 
-		SetMaterial( mat )
+		SetMaterial( pMaterial )
 		DrawTexturedRect( x, y, w, h )
 
 	end
